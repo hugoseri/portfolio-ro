8a16f9812dcd9403cd5545e84a534f44965b9a94
Hugo Serieys <hugo.serieys@hotmail.fr>
2024-12-11 17:37:22 +0100

    Add pdf resume

---
+++ src/components/PdfViewer.tsx
@@ -0,0 +1,36 @@
+import { Loader } from "lucide-react";
+import type React from "react";
+import { Document, Page } from "react-pdf";
+import { pdfjs } from 'react-pdf';
+import './PdfViewer.css'
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
+
+export interface PdfViewerProps {
+  pdfPath: string,
+}
+
+export const PdfViewer: React.FC<PdfViewerProps> = ({
+  pdfPath,
+}) => {
+    
+    return (
+      <div className="pdf-container">
+      <Document 
+        className="rounded-3xl"
+        file={pdfPath} 
+        renderMode="canvas" 
+        loading={<Loader className="m-2 animate-spin"/>}
+      >
+          <Page
+            pageNumber={1} 
+            renderTextLayer={false} 
+            renderAnnotationLayer={false} 
+          />
+      </Document>
+      </div>
+    );
+}
